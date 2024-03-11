4d52f9a978dea05ad486bf9347b65181b19b7b81
Felipe da Costa Ferreira <prog.study.fcf@gmail.com>
2024-03-11 11:12:52 +0000

    Atividade 1

--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>DSWeb - 2024.1 - 20231014040005 - Felipe da Costa Ferreira</h1>")
+    return HttpResponse("<h1>DSWeb - 2024.1</h1><h2>20231014040005 - Felipe da Costa Ferreira</h2>")
