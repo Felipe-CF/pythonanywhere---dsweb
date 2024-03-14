0d4f9b1f30519687a390898e31bf160c3e3c37c5
Felipe da Costa Ferreira <prog.study.fcf@gmail.com>
2024-03-14 13:34:57 +0000

    14/03/2024

--- mysite/enquetes/models.py
+++ mysite/enquetes/models.py
@@ -1,12 +1,21 @@
+from django.utils import timezone
 from django.db import models
+import datetime
 
-# Create your models here.
 
 class Pergunta(models.Model):
     texto= models.CharField(max_length=200)
     data_pub = models.DateTimeField('data de publicação')
-
+    def publicada_recentemente(self):
+        agora = timezone.now()
+        return self.data_pub >= agora - date.timedelta(hours=24)
+     def __str__(self):
+        return "{} ({})".format(self.texto, self.id)
+        
+        
 class Alternativa(models.Model):
     texto= models.CharField(max_length=250)
-    quant_votos= models.IntegerField('quantidade de votos', default=0)
+    quant_votos= models.IntegerField('quantidade de votos', default=
     pergunta_associada= models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+    def __str__(self):
+        return "{} ({})".format(self.texto, self.id)
