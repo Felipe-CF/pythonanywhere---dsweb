02da2c6aa068f9c9d1da1cba529373319430cf14
Felipe da Costa Ferreira <prog.study.fcf@gmail.com>
2024-03-17 18:16:06 +0000

    Primeira atividade

--- mysite/enquetes/models.py
+++ mysite/enquetes/models.py
@@ -9,13 +9,13 @@ class Pergunta(models.Model):
     def publicada_recentemente(self):
         agora = timezone.now()
         return self.data_pub >= agora - date.timedelta(hours=24)
-     def __str__(self):
+    def __str__(self):
         return "{} ({})".format(self.texto, self.id)
-        
-        
+
+
 class Alternativa(models.Model):
     texto= models.CharField(max_length=250)
-    quant_votos= models.IntegerField('quantidade de votos', default=
+    quant_votos= models.IntegerField('quantidade de votos', default=0)
     pergunta_associada= models.ForeignKey(Pergunta, on_delete=models.CASCADE)
     def __str__(self):
         return "{} ({})".format(self.texto, self.id)
