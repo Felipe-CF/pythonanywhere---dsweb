c4d79fd1de40acb7a0ba7c15e1aa5c73ff58db1e
Felipe da Costa Ferreira <prog.study.fcf@gmail.com>
2024-04-04 13:38:33 +0000

    Aula do dia 04-04 - falta fazer a votacao funcionar

--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
-from .models import Pergunta
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Pergunta, Alternativa
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
 
 def index(request):
     lista = Pergunta.objects.all()
@@ -13,6 +15,7 @@ def index(request):
     # return HttpResponse(
     #     "<h1>Disciplina:DSWeb   Período: 2024.1</h1><h2>Matrícula: 20231014040005 - Aluno:Felipe da Costa Ferreira</h2>"
     #     )
+
 
 def detalhes(request, pergunta_id):
     # VERSÃO 1
@@ -30,10 +33,33 @@ def detalhes(request, pergunta_id):
     return render(request, 'enquetes/detalhes.html', contexto)
     # render(request, 'enquetes/detalhes.html', {'enquete': pergunta})
 
+
+
 def votacao(request, pergunta_id):
-    result = "VOTAÇÃO da enquete de número %s"
-    return HttpResponse(result % pergunta_id)
+    pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
+
+    try:
+        id_alternativa = request.POST['escolha']
+        alt = pergunta.alternativa_set.get(pk=id_alternativa)
+    except (KeyError, Alternativa.DoesNotExist):
+        contexto = {
+                'enquetes': pergunta,
+                'error': 'Você precisa selecionar uma alternativa'
+            }
+        return render(request, 'enquetes/detalhes.html', contexto)
+    else:
+        alt.quant_votos += 1
+        alt.save()
+        return HttpResponseRedirect(reverse(
+            'enquetes:resultado', args=(pergunta_id,)
+        ))
+
+
+
 
 def resultado(request, pergunta_id):
-    result = "RESULTADO da enquete de número %s"
-    return HttpResponse(result % pergunta_id)
+    pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
+    return reder(request, )
+
+    # result = "RESULTADO da enquete de número %s"
+    # return HttpResponse(result % pergunta_id)
